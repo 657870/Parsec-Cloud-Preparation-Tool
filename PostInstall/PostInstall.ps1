3c6c9d43714bcbc3783b3e687cadce6da6ca0cef
Martin Roszak <roszak@incas.de>
2021-07-30 11:46:09 +0200

    Update PostInstall.ps1

--- PostInstall/PostInstall.ps1
+++ PostInstall/PostInstall.ps1
@@ -767,8 +767,13 @@ Function provider-specific {
 
 #7Zip is required to extract the Parsec-Windows.exe File
 function Install7Zip {
-    $url = Invoke-WebRequest -Uri https://www.7-zip.org/download.html -UseBasicParsing
-    (New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/$($($($url.Links | Where-Object outertext -Like "Download")[1]).OuterHTML.split('"')[1])" ,"C:\ParsecTemp\Apps\7zip.exe")
+    if ((gwmi win32_operatingsystem | % caption) -like '*Windows 10*') {
+        $url = Invoke-WebRequest -Uri https://www.7-zip.org/download.html -UseBasicParsing
+        (New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/$($($($url.Links | Where-Object outerHTML -Like "*Download*")[1]).outerHTML.split('"')[1])" ,"C:\ParsecTemp\Apps\7zip.exe")
+    } else {
+        $url = Invoke-WebRequest -Uri https://www.7-zip.org/download.html
+        (New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/$($($($url.Links | Where-Object outertext -Like "Download")[1]).OuterHTML.split('"')[1])" ,"C:\ParsecTemp\Apps\7zip.exe")
+    }
     Start-Process C:\ParsecTemp\Apps\7zip.exe -ArgumentList '/S /D="C:\Program Files\7-Zip"' -Wait
     }
 
